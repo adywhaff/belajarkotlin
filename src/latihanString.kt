6a23cd6999335c27958d6e65531eb3cb7edbf294
adywhaff <adywhaffreward>
2017-10-20 15:14:57 +0700

    permata

---
+++ src/latihanString.kt
@@ -0,0 +1,22 @@
+fun main(args: Array<String>) {
+
+
+
+
+        var title="Selamat Datang di Unisnu?"
+        println(title)
+        println("tiltle:"+title)
+        println("tiltle:$title")
+        var name="Akhmad"+"Ajdie"
+        println("name: $name")
+        println("Second Char:"+ title[1])
+        println("tiltle:"+ title.toLowerCase())
+        println("tiltle:"+ title.toUpperCase())
+        println("tiltle:"+ title.split(""))
+        println("tiltle:"+ title.trim())
+
+
+
+
+
+}
